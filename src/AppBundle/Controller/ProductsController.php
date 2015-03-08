11f37ba0c807a9aa1f7752de3facb2ffb5d289be
spibuba <tomaszbubien@gmail.com>
2015-03-08 17:02:14 +0100

    Poczatki pracy nad sklepem

---
+++ src/AppBundle/Controller/ProductsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ProductsController extends Controller
+{
+    
+    /**
+     * @Route("/produkty", name="products_list")
+     */    
+    public function indexAction()
+    {
+        
+        
+        return $this->render('products/index.html.twig', [
+            'products' => $this->getProducts(),
+        ]);
+    }
+    
+    private function getProducts()
+    {
+        $file = file('product.txt'); 
+        $products = array(); 
+        foreach ($file as $p) 
+        { 
+            //explode(znak rozdzielajacy, trim - usuwa biale znaki)
+            $e = explode(':', trim($p)); 
+            $products[$e[0]] = array( 
+                'id' => $e[0], 
+                'name' => $e[1],
+                'price' => $e[2],
+                'desc' => $e[3],
+            ); 
+        }
+        
+        return $products;
+    }
+}
